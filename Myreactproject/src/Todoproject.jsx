cec3186b6216dcc028250f6a0b4a5aba2ce87cc3
Opeyemi <princeolusan9@gmail.com>
2024-06-11 16:58:50 +0100

    edited

--- Myreactproject/src/Todoproject.jsx
+++ Myreactproject/src/Todoproject.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import ReactDOM from 'react-dom/client'
 
 function Todoproject (){
     const[task,settask] = useState([])
     const[newtask,setnewtask]= useState("")
-    const[progress,setprogress]=useState("pending")
+    
 
     function inputtask(event){
    setnewtask(event.target.value)
@@ -20,30 +21,33 @@ function Todoproject (){
   settask(updatedtask);
  }
  function inprogress (){
-    const updatedtask = ("in progress")
-    settask(updatedtask)
- 
+    
  }
  return(
     <div className="Todoproject">
-    <h1>How To Prepare Eba </h1>
+    <p className="text-white text-center display-1 text-decoration-underline text-capitalize">Task Recorder</p>
     <div>
-        <input type="text"
+        <input type="text" className="p-2 rounded border-0 me-2"
             placeholder="Add your task here"
             value={newtask}
             onChange={inputtask}
         />
-        <button className="addbutton" onClick={addtask}>
-            
+        <button className="addbutton p-2 btn-light btn" onClick={addtask}>
+           Add 
         </button>
     </div>
     <ol>
         {task.map((task,index)=> 
-        <li key={index}>
-            <p className="">{task}</p>
-          <button className="" onClick={()=>deletetask(index)}>Done</button>
-          <button className="" onClick={()=>inprogress(index)}>in progress</button>
-        </li>
+      <ol key={index} className="d-flex justify-content-center">
+            <li className="ms-2 me-4 mt-3 fs-3 text-white">{task}</li>
+            <select name="" id="d1"  className=" mt-3 me-3">      
+                    <option className="" onInput={()=>deletetask(index)}>Done</option>
+          <option className="" onInput={()=>inprogress()}>in progress</option>
+    
+          </select>
+                   <button  onClick={()=>deletetask(index)} className=" mt-2">UPDATE</button>
+        </ol>
+     
         )}
     </ol>
     </div>
